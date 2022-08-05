5127c742efdf6bbbbaa81b712001eeabfc22104f
wangsungtaek <tjdxorv@naver.com>
2022-08-06 00:25:15 +0900

    add Image

--- src/components/imageSlider.vue
+++ src/components/imageSlider.vue
@@ -17,7 +17,7 @@
               :gap="3"
               slide-multiple
               :dragging-distance="70">
-              <vueper-slide v-for="imgPath in imageList" :key="imgPath" :image="imgPath" @click.native="test(imgPath)" />
+              <vueper-slide v-for="imgPath in imageList" :key="imgPath.path" :image="imgPath.path" @click.native="move(imgPath.link)" />
             </vueper-slides>
           </div>
         </div>
@@ -32,16 +32,30 @@ export default {
   components: { VueperSlides, VueperSlide },
   created() {
     for (let i = 1; i <= 22; i++) {
-      this.imageList[i-1] = require(`@/assets/images/example/example_${i}.jpg`);
+      this.imageList[i-1] = {
+        path: require(`@/assets/images/example/example_${i}.jpg`),
+        link: this.link[i-1]
+      };
     }
   },
   data: () => ({
-    imageList: []
+    imageList: [],
+    link: [
+      "https://blog.naver.com/ejrnfdl6/222812512676",         /* 광동어린이젤리스틱 */
+      "https://blog.naver.com/lael_v_v/222699406411",         /* 빌드림 */
+      "https://blog.naver.com/son8199/222829423053",          /* 호식이두마리치킨 */
+      "https://blog.naver.com/3jwhjhy/222698990110",          /* 닥터그루트 */
+      "https://blog.naver.com/gittygittygitty/222750596580",  /* 아라제이 */
+      "https://www.instagram.com/p/CdfW8A_Pes2",              /* 마크제이콥스 */
+      "https://www.instagram.com/p/CfjRBd9PT8e/",             /* 장수촌 */
+      "https://www.instagram.com/p/CVUPD42ps3B/",             /* 월화고기 */
+    ]
   }),
   methods: {
-    test(ev, i) {
-      console.log(ev)
-      console.log(i)
+    move(path) {
+      if(path) {
+        window.open(path);
+      }
     }
   }
 }
